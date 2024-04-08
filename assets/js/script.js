82102f4028369559b4f90930999fdb2e942f399a
Fel <felgonzalez951@gmail.com>
2024-04-08 16:02:17 -0600

    added formsubmithandler function, needs to be finished. added getcityweather function, needs to be finished

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,24 @@ const APIKey = 'f12afd4f4aecb8e77f094912f64ed517';
 const cityFormEl = document.querySelector('#city-form');
 const nameInputEl = document.querySelector('#cityname');
 
+
+const formSubmitHandler = function (event) {
+    event.preventDefault();
+
+    const cityname = nameInputEl.value.trim();
+
+    if (cityname) {
+
+    }
+}
+
+// =========== api stuff ========
 let city;
 
-const queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
 
-fetch(queryURL)
+const getCityWeather = function (city) {
+    const queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
+    
+
+    fetch(queryURL)
+}
