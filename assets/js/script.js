743e843b67ece7a58d3941726c164263df26c6bc
Fel <felgonzalez951@gmail.com>
2024-04-08 15:25:43 -0600

    added queryURL from 'how to use api keys' full-stack blog help

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,7 @@
-const APIKey = 'f12afd4f4aecb8e77f094912f64ed517'
+const APIKey = 'f12afd4f4aecb8e77f094912f64ed517';
+
+// * https://api.openweathermap.org/data/2.5/weather?q={city}&appid={APIkey}
+
+let city;
+
+const queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
