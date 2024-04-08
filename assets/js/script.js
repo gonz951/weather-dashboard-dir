1dca8a5113c37b3476acff614facc39e018cf993
Fel <felgonzalez951@gmail.com>
2024-04-08 15:43:18 -0600

    added consts to script concerning the form and city name input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
 const APIKey = 'f12afd4f4aecb8e77f094912f64ed517';
-
-// * https://api.openweathermap.org/data/2.5/weather?q={city}&appid={APIkey}
+const cityFormEl = document.querySelector('#city-form');
+const nameInputEl = document.querySelector('#cityname');
 
 let city;
 
 const queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
+
+fetch(queryURL)
